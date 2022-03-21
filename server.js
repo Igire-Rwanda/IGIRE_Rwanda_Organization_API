2bb5e66f223255fd8791eda4118a0aab16c3217d
christale <christalebu@gmail.com>
2022-03-21 03:06:27 -0700

    igire backend project

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import cors from "cors"
+dotenv.config();
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+app.use("/",(req,res) => res.status(200).json({
+    message:"this is api"
+}));
+const dburl=process.env.DATABASEURL;
+
+mongoose.connect(dburl,{
+    
+} ).then(()=>console.log("database connected successfully"))
+const port=process.env.PORT
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
+})
+
+
+export default app;
